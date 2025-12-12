6cd755001f75e4abe820acd7dee90408ccac3d33
Rajan9865 <rajan78800@gmail.com>
2025-12-12 17:59:38 +0530

    order management service

--- order-management-service/order-management-service/src/main/java/com/practice/oms/OrderManagementServiceApplication.java
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/OrderManagementServiceApplication.java
@@ -1,4 +1,4 @@
-package com.practice.order_management_service;
+package com.practice.oms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
