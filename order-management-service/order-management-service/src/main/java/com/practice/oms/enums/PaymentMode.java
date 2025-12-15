4eb62c9be64c633910f3a4c0401089e72774a5ee
Rajan9865 <rajan78800@gmail.com>
2025-12-15 17:37:14 +0530

    order management service

---
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/enums/PaymentMode.java
@@ -0,0 +1,14 @@
+package com.practice.oms.enums;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Practise_Project
+ * @since 12/15/2025
+ */
+public enum PaymentMode {
+    UPI,
+    CARD,
+    NET_BANKING,
+    CASH
+}
