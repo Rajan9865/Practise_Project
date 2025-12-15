25baa5ac79ebcdad9984e9a1e770ea76a2423b37
Rajan9865 <rajan78800@gmail.com>
2025-12-15 16:15:04 +0530

    order management service

---
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/entity/OrderItem.java
@@ -0,0 +1,49 @@
+package com.practice.oms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Practise_Project
+ * @since 12/15/2025
+ */
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Parent order.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    /**
+     * Product being ordered.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    /**
+     * Quantity ordered.
+     */
+    private Integer quantity;
+
+    /**
+     * Product price at order time.
+     */
+    private BigDecimal priceAtOrderTime;
+}
---
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Payment.java
@@ -0,0 +1,10 @@
+package com.practice.oms.entity;
+
+/**
+ * @author lenovo
+ * @version 1.0
+ * Practise_Project
+ * @since 12/15/2025
+ */
+public class Payment {
+}
