f53e95a763464772ad557ba9d778ed23113054f7
Rajan9865 <rajan78800@gmail.com>
2025-12-15 17:36:36 +0530

    order management service

--- order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Address.java
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Address.java
@@ -1,17 +1,23 @@
 package com.practice.oms.entity;
 
+import com.practice.oms.enums.AddressType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @author lenovo
+ * Address Entity
+ *
+ * <p>
+ * This class represents an address associated with a customer.
+ * A customer can have multiple addresses such as HOME or OFFICE.
+ * </p>
+ *
+ * @author Rajan Kumar
  * @version 1.0
- * Practise_Project
- * @since 12/15/2025
+ * @since 15-12-2024
  */
-
 @Entity
 @Table(name = "addresses")
 @Data
@@ -24,19 +30,42 @@ public class Address {
 
     /**
      * Customer to whom this address belongs.
+     * Many addresses can be associated with one customer.
      */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
+    /**
+     * Primary address line (House no, Street, Area).
+     */
+    @Column(name = "address_line_1", nullable = false)
     private String addressLine1;
+    /**
+     * Secondary address line (optional).
+     */
+    @Column(name = "address_line_2")
     private String addressLine2;
+    /**
+     * City name.
+     */
+    @Column(nullable = false)
     private String city;
+    /**
+     * State name.
+     */
+    @Column(nullable = false)
     private String state;
+    /**
+     * Postal / PIN code.
+     */
+    @Column(nullable = false)
     private String pincode;
 
     /**
-     * HOME / OFFICE
+     * Type of address (HOME / OFFICE).
      */
-    private String addressType;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "address_type", nullable = false)
+    private AddressType addressType;
 }
