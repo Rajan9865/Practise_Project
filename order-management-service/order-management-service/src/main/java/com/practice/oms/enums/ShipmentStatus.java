6174518bcf5218e348d7a7d3e3369624fa0b6064
Rajan9865 <rajan78800@gmail.com>
2025-12-15 17:37:33 +0530

    order management service

---
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/enums/ShipmentStatus.java
@@ -0,0 +1,15 @@
+package com.practice.oms.enums;
+
+/**
+ * @author Rajan kumar
+ * @version 1.0
+ * Practise_Project
+ * @since 12/15/2025
+ */
+public enum ShipmentStatus {
+    CREATED,
+    SHIPPED,
+    IN_TRANSIT,
+    DELIVERED,
+    RETURNED
+}
