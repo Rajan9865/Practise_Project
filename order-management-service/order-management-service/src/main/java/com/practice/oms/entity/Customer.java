946a522625ebec74fefe996fae1712afd16d778f
Rajan9865 <rajan78800@gmail.com>
2025-12-15 12:10:11 +0530

    order management service

--- order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Customer.java
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Customer.java
@@ -20,8 +20,14 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name",nullable = false)
     private String firstName;
+
+    @Column(name = "last_name ", nullable = false)
     private String lastName;
+
+    @Column(nullable = false,unique = true)
     private String email;
 
 }
