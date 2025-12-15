adcf26180ab2b842cb4643b9edf370f9bf2f1a8b
Rajan9865 <rajan78800@gmail.com>
2025-12-15 16:18:12 +0530

    order management service

--- order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Payment.java
+++ order-management-service/order-management-service/src/main/java/com/practice/oms/entity/Payment.java
@@ -1,10 +1,35 @@
 package com.practice.oms.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * @author lenovo
  * @version 1.0
  * Practise_Project
  * @since 12/15/2025
  */
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private String paymentMode;
+    private String paymentStatus;
+    private String transactionId;
+
+    private LocalDateTime paymentTime;
 }
