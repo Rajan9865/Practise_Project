f93427d3b65c09e238cc81373e3a7a3f5ace4a7e
Rajan9865 <rajan78800@gmail.com>
2026-07-20 10:56:48 +0530

    UserManagementSystemApplication

--- user-management-system/src/main/java/com/usermanagement/user_management_system/controller/UserController.java
+++ user-management-system/src/main/java/com/usermanagement/user_management_system/controller/UserController.java
@@ -3,9 +3,17 @@ package com.usermanagement.user_management_system.controller;
 import com.usermanagement.user_management_system.dto.UserRequestDto;
 import com.usermanagement.user_management_system.dto.UserResponseDto;
 import com.usermanagement.user_management_system.service.UserService;
-import com.usermanagement.user_management_system.util.ApiResponse;
+import com.usermanagement.user_management_system.util.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +26,8 @@ import java.util.List;
  * Practise_Project
  * @since 7/16/2026
  */
+@SecurityRequirement(name="basicAuth")
+@Tag(name = "User API",description = "Operations related to User Management")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -29,10 +39,15 @@ public class UserController {
      * Create User
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<UserResponseDto>> createUser(
+    @Operation( summary = "Create User",description = "Creates a new user in the system.")
+    @ApiResponses({@ApiResponse( responseCode = "201",description = "User created successfullyhhh"),
+            @ApiResponse(responseCode = "400",description = "Validation failed"),
+            @ApiResponse(responseCode = "409",description = "Username or email already exists")
+    })
+    public ResponseEntity<ApiResult<UserResponseDto>> createUser(
             @Valid @RequestBody UserRequestDto request) {
         UserResponseDto response = userService.createUser(request);
-        ApiResponse<UserResponseDto> apiResponse = ApiResponse.<UserResponseDto>builder()
+        ApiResult<UserResponseDto> apiResponse = ApiResult.<UserResponseDto>builder()
                 .success(true)
                 .message("User created successfully.")
                 .data(response)
@@ -44,11 +59,13 @@ public class UserController {
     /**
      * Get User By Id
      */
+
+    @Operation(summary = "Get User By Id")
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponseDto>> getUserById(
+    public ResponseEntity<ApiResult<UserResponseDto>> getUserById(
             @PathVariable Long id) {
         UserResponseDto response = userService.getUserById(id);
-        ApiResponse<UserResponseDto> apiResponse = ApiResponse.<UserResponseDto>builder()
+        ApiResult<UserResponseDto> apiResponse = ApiResult.<UserResponseDto>builder()
                 .success(true)
                 .message("User fetched successfully.")
                 .data(response)
@@ -59,27 +76,48 @@ public class UserController {
     /**
      * Get All Users
      */
+//    @Operation(summary = "Get All Users")
+//    @GetMapping
+//    public ResponseEntity<ApiResult<List<UserResponseDto>>> getAllUsers() {
+//        List<UserResponseDto> response = userService.getAllUsers();
+//        ApiResult<List<UserResponseDto>> apiResponse =
+//                ApiResult.<List<UserResponseDto>>builder()
+//                        .success(true)
+//                        .message("Users fetched successfully.")
+//                        .data(response)
+//                        .build();
+//        return ResponseEntity.ok(apiResponse);
+//    }
+    @Operation(summary = "get all users")
     @GetMapping
-    public ResponseEntity<ApiResponse<List<UserResponseDto>>> getAllUsers() {
-        List<UserResponseDto> response = userService.getAllUsers();
-        ApiResponse<List<UserResponseDto>> apiResponse =
-                ApiResponse.<List<UserResponseDto>>builder()
-                        .success(true)
-                        .message("Users fetched successfully.")
-                        .data(response)
-                        .build();
-        return ResponseEntity.ok(apiResponse);
+    public ResponseEntity<ApiResult<Page<UserResponseDto>>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+        Sort sort = direction.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+        PageRequest pageable = PageRequest.of(page, size, sort);
+        Page<UserResponseDto> users = userService.getallUsers(pageable);
+        ApiResult<Page<UserResponseDto>> response = ApiResult.<Page<UserResponseDto>>builder()
+                .success(true)
+                .message("Users fetched successfully.")
+                .data(users)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     /**
      * Update User
      */
+    @Operation(summary = "Update User")
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponseDto>> updateUser(
+    public ResponseEntity<ApiResult<UserResponseDto>> updateUser(
             @PathVariable Long id,
             @Valid @RequestBody UserRequestDto request) {
         UserResponseDto response = userService.updateUser(id, request);
-        ApiResponse<UserResponseDto> apiResponse = ApiResponse.<UserResponseDto>builder()
+        ApiResult<UserResponseDto> apiResponse = ApiResult.<UserResponseDto>builder()
                 .success(true)
                 .message("User updated successfully.")
                 .data(response)
@@ -90,9 +128,29 @@ public class UserController {
     /**
      * Delete User
      */
+    @Operation(summary = "Delete User")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     *
+     * @param username
+     * @return Search user
+     */
+    @Operation(summary = "search users")
+    @GetMapping("search")
+    public ResponseEntity<ApiResult<List<UserResponseDto>>> searchUsers(
+            @RequestParam String username
+    ) {
+        List<UserResponseDto> user = userService.searchUsers(username);
+        ApiResult<List<UserResponseDto>> response = ApiResult.<List<UserResponseDto>>builder()
+                .success(true)
+                .message("Users fetched successfully.")
+                .data(user)
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
